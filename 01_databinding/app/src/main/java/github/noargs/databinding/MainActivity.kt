52d8e9c8fd6500ddec798fd5c9063488c3dc79b9
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-06 17:08:15 +0000

    findViewById replaced with dataBinding

--- 01_databinding/app/src/main/java/github/noargs/databinding/MainActivity.kt
+++ 01_databinding/app/src/main/java/github/noargs/databinding/MainActivity.kt
@@ -2,24 +2,37 @@ package github.noargs.databinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+//import android.widget.Button
+//import android.widget.EditText
+//import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import github.noargs.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    val button = findViewById<Button>(R.id.submit_button)
-    button.setOnClickListener {
+//    setContentView(R.layout.activity_main)
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+//    val button = findViewById<Button>(R.id.submit_button)
+    binding.submitButton.setOnClickListener {
       displayGreeting()
     }
   }
 
   private fun displayGreeting() {
-    val messageView = findViewById<TextView>(R.id.greeting_text_view)
-    val nameText = findViewById<EditText>(R.id.name_edit_text)
+//    val messageView = findViewById<TextView>(R.id.greeting_text_view)
+//    val nameText = findViewById<EditText>(R.id.name_edit_text)
 
-    messageView.text = "Hello! "+ nameText.text
+    binding.greetingTextView.text = "Hello! "+ binding.nameEditText.text
+
+    // use the kotlin `apply` scoping function to reduce the repetition of
+    // name of the databinding function
+//    binding.apply {
+//      greetingTextView.text = "Hello! " + nameEditText.text
+//    }
   }
 }
