f71eb27e7bafc55f84f1262901c29abf3a2f32b9
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-06 23:53:02 +0000

    counter logic moved into MainActivityViewModel to preserve configuration changes

--- 03_viewmodel/app/src/main/java/github/noargs/viewmodel/MainActivity.kt
+++ 03_viewmodel/app/src/main/java/github/noargs/viewmodel/MainActivity.kt
@@ -3,24 +3,25 @@ package github.noargs.viewmodel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import github.noargs.viewmodel.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMainBinding
-  private var count = 0
+  private lateinit var viewModel: MainActivityViewModel
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
 //    setContentView(R.layout.activity_main)
 
     binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
 
-    binding.countText.text = count.toString()
+    binding.countText.text = viewModel.getCurrentCount().toString()
 
     binding.button.setOnClickListener {
-      count++
-      binding.countText.text = count.toString()
+      binding.countText.text = viewModel.getUpdatedCount().toString()
     }
   }
 }
--- 03_viewmodel/app/src/main/java/github/noargs/viewmodel/MainActivityViewModel.kt
+++ 03_viewmodel/app/src/main/java/github/noargs/viewmodel/MainActivityViewModel.kt
@@ -3,5 +3,13 @@ package github.noargs.viewmodel
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel: ViewModel() {
-  
+  private var count = 0
+
+  fun getCurrentCount(): Int {
+    return count
+  }
+
+  fun getUpdatedCount(): Int {
+    return ++count
+  }
 }
