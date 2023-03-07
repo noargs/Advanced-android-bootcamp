ae45b25710ade38e2acc04f5f95c783d38ae957a
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-07 03:04:24 +0000

    viewmodel/databinding wired with views

--- 03_viewmodel_exercise/app/src/main/java/github/noargs/viewmodelexercise/MainActivity.kt
+++ 03_viewmodel_exercise/app/src/main/java/github/noargs/viewmodelexercise/MainActivity.kt
@@ -2,10 +2,33 @@ package github.noargs.viewmodelexercise
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import github.noargs.viewmodelexercise.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+  private lateinit var viewModel: MainActivityViewModel
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+//    setContentView(R.layout.activity_main)
+
+    binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+
+    binding.resultTextView.text = viewModel.getTotal().toString()
+
+    binding.insertButton.setOnClickListener {
+      if (binding.inputEditText.text.toString() == "") {
+        Toast.makeText(this, "Please enter a number", Toast.LENGTH_LONG).show()
+      } else {
+        viewModel.setTotal(binding.inputEditText.text.toString().toInt())
+        binding.resultTextView.text = viewModel.getTotal().toString()
+        binding.inputEditText.text.clear()
+      }
+    }
   }
 }
---
+++ 03_viewmodel_exercise/app/src/main/java/github/noargs/viewmodelexercise/MainActivityViewModel.kt
@@ -0,0 +1,16 @@
+package github.noargs.viewmodelexercise
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+  private var total = 0
+
+  fun getTotal(): Int {
+    return total
+  }
+
+  fun setTotal(input: Int) {
+    total += input
+  }
+}
