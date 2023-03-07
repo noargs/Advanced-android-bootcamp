303515025e1f8c1c4c4c1daa5b74c71fe9ef55b0
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-07 19:33:15 +0000

    ViewModel incorporated in View Mode Object i.e. in xml with the help of dataBinding

--- 08_viewmodel_with_databinding/app/src/main/java/github/noargs/viewmodelwithdatabinding/MainActivity.kt
+++ 08_viewmodel_with_databinding/app/src/main/java/github/noargs/viewmodelwithdatabinding/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import github.noargs.viewmodelwithdatabinding.databinding.ActivityMainBinding
 
-// Integrate DataBinding directly in View model object i.e. Activity xml file
+// Integrate DataBinding directly in View model object i.e. in Activity xml file
 class MainActivity : AppCompatActivity() {
 
   private lateinit var binding: ActivityMainBinding
@@ -19,22 +19,15 @@ class MainActivity : AppCompatActivity() {
 
     binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+    binding.viewModel = viewModel
 
     viewModel.count.observe(this, Observer {
       binding.textViewCounter.text = it.toString()
     })
 
-    // - Replaced by above code `observer` from liveData
-    // binding.textViewCounter.text = viewModel.getCurrentCount().toString()
-
-    binding.buttonClick.setOnClickListener {
-
-      // binding.textViewCounter.text = viewModel.getUpdatedCount().toString()
-
-      // - above code commented as we only tell the viewModel to increment
-      // - and result i.e. updated `count` automatically gets changed in `textViewCounter`
-      // - as it's been observed by LiveData
-      viewModel.updateCount()
-    }
+// - [[[ Function setOnClickListener is moved to activity_main.xml]]]
+//    binding.buttonClick.setOnClickListener {
+//      viewModel.updateCount()
+//    }
   }
 }
