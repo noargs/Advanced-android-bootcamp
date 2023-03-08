e426fd02a1cc49d377decb1f3809a0149e4a688c
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-08 22:18:29 +0000

    init exercise project with dependecies and dataBinding ON

---
+++ 13_navigation_architecure_components_exercise/app/src/main/java/github/noargs/navigationexercise/MainActivity.kt
@@ -0,0 +1,11 @@
+package github.noargs.navigationexercise
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
