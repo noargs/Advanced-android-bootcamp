d702a70c9ed28de31e404f06480eec4023793824
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-14 06:34:18 +0000

    lifeCycleScope with progressBar and using in IO background thread

--- 21_lifecyclescope/app/src/main/java/github/noargs/lifecyclescopebyktx/MainActivity.kt
+++ 21_lifecyclescope/app/src/main/java/github/noargs/lifecyclescopebyktx/MainActivity.kt
@@ -2,7 +2,14 @@ package github.noargs.lifecyclescopebyktx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
+import androidx.lifecycle.lifecycleScope
 import github.noargs.lifecyclescopebyktx.ui.main.MainFragment
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 // Another scope introduced by google is LifeCycleScope
 // LifeCycleScope is defined by each lifeCycle Object
@@ -20,13 +27,43 @@ import github.noargs.lifecyclescopebyktx.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var progressBar: ProgressBar
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
     if (savedInstanceState == null) {
       supportFragmentManager.beginTransaction()
         .replace(R.id.container, MainFragment.newInstance())
         .commitNow()
     }
+
+    progressBar = findViewById(R.id.progressBar)
+
+//    lifecycleScope.launch {// if you want it to run in separate thread the use `lifecycleScope.launch(Dispatchers.IO)`
+//      delay(5000)
+//      progressBar.visibility = View.VISIBLE
+//      delay(3000)
+//      progressBar.visibility = View.GONE
+//      delay(4000)
+//      progressBar.visibility = View.VISIBLE
+//      delay(3000)
+//      progressBar.visibility = View.GONE
+//    }
+
+
+    lifecycleScope.launch(Dispatchers.IO) {
+      Log.i("MyTag", " thread is ${Thread.currentThread().name}")
+    }
+
+  // Sometimes we might need to suspend the execution of a code block, considering
+  //    the current state of a lifecycle
+  // For that we have three additional builders - NOW Deprecated
+    lifecycleScope.launchWhenCreated {} // when activity or fragment created
+    lifecycleScope.launchWhenStarted {} // when activity or fragment started
+    lifecycleScope.launchWhenResumed { } // when activity or fragment resumed
+
   }
-}
+
+} // end of
--- 21_lifecyclescope/app/src/main/java/github/noargs/lifecyclescopebyktx/ui/main/MainFragment.kt
+++ 21_lifecyclescope/app/src/main/java/github/noargs/lifecyclescopebyktx/ui/main/MainFragment.kt
@@ -2,11 +2,15 @@ package github.noargs.lifecyclescopebyktx.ui.main
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import github.noargs.lifecyclescopebyktx.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainFragment : Fragment() {
 
@@ -20,6 +24,10 @@ class MainFragment : Fragment() {
     super.onCreate(savedInstanceState)
     viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     // TODO: Use the ViewModel
+
+    lifecycleScope.launch(Dispatchers.IO) {
+      Log.i("MyTag", " thread is ${Thread.currentThread().name}")
+    }
   }
 
   override fun onCreateView(
