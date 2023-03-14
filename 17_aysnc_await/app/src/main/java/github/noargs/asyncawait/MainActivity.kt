6e24e3ec428b3c78b0ca094a44e061042870385c
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-14 00:30:50 +0000

    using async with coroutine in same and async with coroutine in different threads

--- 17_aysnc_await/app/src/main/java/github/noargs/asyncawait/MainActivity.kt
+++ 17_aysnc_await/app/src/main/java/github/noargs/asyncawait/MainActivity.kt
@@ -3,7 +3,8 @@ package github.noargs.asyncawait
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import kotlinx.coroutines.delay
+import android.widget.Toast
+import kotlinx.coroutines.*
 
 
 // Let's assume we have to get results from 4 different online data sources
@@ -32,6 +33,44 @@ class MainActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    // [[ Sequential Decomposition ]] - will take 18s
+//    CoroutineScope(Dispatchers.IO).launch {
+//      Log.i("MyTag", "Calculations started....")
+//      val stock1 = getStock1()
+//      val stock2 = getStock2()
+//      val total = stock1 + stock2
+//      Log.i("MyTag", "Total is $total")
+//    }
+
+
+    // [[ Parallel Decomposition ]] -  will take 10s only
+//    CoroutineScope(Dispatchers.IO).launch {
+//      Log.i("MyTag", "Calculations started....")
+//
+//      val stock1 = async { getStock1() }
+//      val stock2 = async { getStock2() }
+//      val total = stock1.await() + stock2.await()
+//
+//      Log.i("MyTag", "Total is $total")
+//    }
+
+    // [[ Parallel Decomposition ]] -  will take 10s only
+    CoroutineScope(Dispatchers.Main).launch {// this will run in Main (UI) Thread
+      Log.i("MyTag", "Calculations started....")
+
+      val stock1 = async(Dispatchers.IO) { getStock1() } // this will run in IO (background thread)
+      val stock2 = async(Dispatchers.IO) { getStock2() } // this will run in IO (background thread)
+      val total = stock1.await() + stock2.await()
+
+      // as we are running `async` in IO thread whereas results (stock1, stock2) are
+      // available in Main thread therefore we can show Toast message which also belong
+      // to Main thread
+      Toast.makeText(applicationContext, "Total is $total", Toast.LENGTH_LONG).show()
+
+      Log.i("MyTag", "Total is $total")
+    }
+
   }
 }
 
@@ -43,7 +82,7 @@ private suspend fun getStock1(): Int {
 }
 
 private suspend fun getStock2(): Int {
-  delay(80000) // 8 seconds
+  delay(8000) // 8 seconds
   Log.i("MyTag", " stock 2 returned")
   return 35000
 }
