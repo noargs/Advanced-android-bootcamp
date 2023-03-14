387226122af04d07af84e4a64b2b15e30a4b0d26
Ibn Asghar <asim.ibn.asghar@gmail.com>
2023-03-14 01:33:49 +0000

    unstructured concurrency demo aka bad way to handle

--- 18_unstructured_concurency/app/src/main/java/github/noargs/unstructuredconcurency/MainActivity.kt
+++ 18_unstructured_concurency/app/src/main/java/github/noargs/unstructuredconcurency/MainActivity.kt
@@ -10,6 +10,23 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+
+// There are situations where we need to run more than 1 coroutine concurrently
+//  in a suspended functions and get some results returned from the function
+// There are two ways to do this, we call them `structured concurrency` and
+//  `unstructured concurrency`
+
+// in this app we will demonstrate `unstructured concurrency` which is the wrong
+//  way to do it
+
+// [[ Unstructured concurrency ]] does not guarantee to complete all the tasks
+//  of the suspending function, before it returns
+// Actually the child's coroutine can be still running even after the completion of
+//  of parent coroutine, as a result we have unpredictable results
+//  especially if we use `launch { }` coroutine builder
+// If we use `async` builder and `await` function call for the return value, you might
+//  be able to get the expected result of the async coroutine
+
 class MainActivity : AppCompatActivity() {
 
   private var count = 0
@@ -17,6 +34,7 @@ class MainActivity : AppCompatActivity() {
   private lateinit var btnDownloadUserData : Button
   private lateinit var btnCount : Button
   private lateinit var tvCount : TextView
+  private lateinit var tvUserMessage: TextView
 
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,19 +44,23 @@ class MainActivity : AppCompatActivity() {
     btnDownloadUserData = findViewById(R.id.btnDownloadUserData)
     btnCount = findViewById(R.id.btnCount)
     tvCount = findViewById(R.id.tvCount)
+    tvUserMessage = findViewById(R.id.tvUserMessage)
 
     btnCount.setOnClickListener {
       tvCount.text = count++.toString()
     }
     btnDownloadUserData.setOnClickListener {
 
-      // `launch` is a builder which launches new coroutine without blocking
-      // the current thread.
-      // Returns an instance of Job, which can be used as a reference to the coroutine
-      // we use `launch` builder for any coroutines that doesn't have any result
-      // as the return value
-      CoroutineScope(Dispatchers.IO).launch {
-        downloadUserData()
+
+      CoroutineScope(Dispatchers.Main).launch {
+
+        // we got 0 as result whereas we have 50 as a value in UserDataManager() class we created
+        // Reason is in UserDataManager() coroutine scope create new coroutine through CoroutineScope(IO)
+        //  which behaves separately from this parent coroutine in the MainActivity
+        // So the function getTotalUserCount() reaches to the end (from count = 0 ...... return count)
+        // without the completion of CoroutineScope (denoted by ..... in above line) (
+        // Therefore unable to change count value from 0 to 50 inside CoroutineScope in UserDataManager class
+        tvUserMessage.text = UserDataManager().getTotalUserCount().toString()
       }
     }
   }
@@ -47,10 +69,6 @@ class MainActivity : AppCompatActivity() {
     for (i in 1..200000) {
       Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
 
-      // to prevent logcat: Unexpected EOF error causes by logcat unable to
-      // catch up with all the log messages generating in high speed by a device
-      // OR device shutdown OR logd crashes
-      // you have to use `suspend` keyword with downloadUserData() function
       delay(100)
     }
   }
